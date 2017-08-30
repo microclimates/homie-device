c60fdb080cf9e1c4734b171e94ea3a3d9d82f79d
Loren West <github@lorenwest.com>
2017-08-30 14:52:31 -0700

    Initial commit

---
+++ test/homieDevice.js
@@ -0,0 +1,12 @@
+const expect    = require("chai").expect;
+const Homie = require('..');
+
+describe("Homie Device", function() {
+  describe("Instantiation", function() {
+    let testDevice = new Homie('bare-minimum', '1.0.0');
+    it("creates a class of the correct type", function() {
+      expect(testDevice).to.be.an.instanceOf(Homie);
+    });
+  });
+
+});
