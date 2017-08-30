12d4cf3f4aa8c0ea9ecbf6b50513fc9ae602d06c
Loren West <github@lorenwest.com>
2017-08-30 15:45:29 -0700

    Fixed tests to run on 4.x node

--- test/homieDevice.js
+++ test/homieDevice.js
@@ -3,7 +3,7 @@ const Homie = require('..');
 
 describe("Homie Device", function() {
   describe("Instantiation", function() {
-    let testDevice = new Homie('bare-minimum', '1.0.0');
+    var testDevice = new Homie('bare-minimum', '1.0.0');
     it("creates a class of the correct type", function() {
       expect(testDevice).to.be.an.instanceOf(Homie);
     });
